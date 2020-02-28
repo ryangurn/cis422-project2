1366a8d5d189f41e0418c893043bda79a4f1f652
ryangurn <ryangurnick@gmail.com>
2020-02-27 18:32:14 -0800

    Continuing work on init

--- RequirementsParser.py
+++ RequirementsParser.py
@@ -22,10 +22,13 @@ class RequirementsParser:
     def __init__(self):
         content = requests.get("http://catalog.uoregon.edu/arts_sciences/computerandinfoscience/#degreeplantext")
         parser = Parser.Parser()
-        parser.feed(content.content)
+        parser.feed(str(content.content))
+
+        self._parser = parser
+        self._tables = self._parser.tables
 
     def deleteFormatting(self):
         pass
 
-    def parseData(self, parser: Parser):
+    def parseData(self):
         pass
