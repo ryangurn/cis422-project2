7c750717770b1cbdfcb4558ac3f078290d75a9c7
ryangurn <ryangurnick@gmail.com>
2020-02-27 23:05:13 -0800

    Adding ClassModel

---
+++ ClassModel.py
@@ -0,0 +1,91 @@
+"""
+ClassModel.py will act as an ORM for the class table in the sqlite database
+to the objects that we will use within this application. This will attempt
+to make querying the table much simpler.
+
+Authors:
+(RegTools)
+Joseph Goh
+Mason Sayyadi
+Owen McEvoy
+Ryan Gurnick
+Samuel Lundquist
+
+Priority credit to:
+Ryan Gurnick - 2/27/20  Creation
+
+"""
+import Datastore
+import sqlite3
+from datetime import date, datetime
+
+class ClassModel:
+
+
+    table = None
+    output = None
+
+    def __init__(self, db_file):
+        """
+        Initializer for the class model. This will want to know which file to look at for the db.
+        Then it will provide a connection to that database and allow for some ORM opterations, such
+        as insert and select.
+
+        db_file :str
+
+        Example Usage:
+        cm = ClassModel.ClassModel('testing.db')
+        """
+        # init db connection using the datastore
+        self.conn = Datastore.DB(db_file).ret().conn
+
+    def insert(self, crn, term, type, seats, sections):
+        """
+        Insert method for the class model that allows the caller to provide the specified
+        information and create a record within the database, this will return the id of
+        the row created.
+
+        :param
+        crn :int
+        term :int
+        type :str
+        seats :str
+        sections :str
+
+        return :int
+
+        Example Usage:
+        cm = ClassModel.ClassModel('testing.db')
+        cm.insert(321321, 201901, '123', '{}', '{}')
+        """
+        sql = '''INSERT INTO "main"."classes" ("crn", "term", "type", "seats", "sections", "created_at", "updated_at") VALUES (?, ?, ?, ?, ?, ?, ?);'''
+        cur = self.conn.cursor()
+        try:
+            cur.execute(sql, (crn, term, type, seats, sections, date.today(), date.today()))
+            self.conn.commit()
+            return cur.lastrowid
+        except sqlite3.IntegrityError:
+            return None
+
+    def find_by(self, haystack, needle):
+        """
+        Search for records within the class database and return them as lists of lists.
+
+        :param
+        haystack :str
+        needle :str
+
+        return :list
+
+        Example Usage:
+        cm = ClassModel.ClassModel('testing.db')
+        cm.find_by('term', 201901)
+        cm.find_by('crn', 321321)
+        """
+        sql = "SELECT * FROM \"main\".\"classes\" WHERE \"{}\" = {}".format(haystack, needle)
+        cur = self.conn.cursor()
+        try:
+            cur.execute(sql)
+        except ValueError:
+            return cur.fetchall()
+        return cur.fetchall()
