c919e78618dfa44c42536832e4a13e44d6882e1f
ryangurn <ryangurnick@gmail.com>
2020-03-05 16:56:04 -0800

    Fixing spacing issues

--- GUI/MainMenu.py
+++ GUI/MainMenu.py
@@ -27,10 +27,10 @@ class MainMenu(tk.Tk):
         self.numberOfNames = 0
 
         # Dark grey background box
-        grayBoxFrame = Frame(master, width = 800, height = 390, bg=self._darkGrey)
-        grayBoxFrame.place(x = 0, y = 150)
+        grayBoxFrame = Frame(master, width=800, height=390, bg=self._darkGrey)
+        grayBoxFrame.place(x=0, y=150)
         fr = Frame(master, width=200, height=200, bg=self._green)
-        fr.place(x=300,y=252)
+        fr.place(x=300, y=252)
 
         # Creates Menu Bar
         menubar = Menu(self.master)
@@ -39,42 +39,40 @@ class MainMenu(tk.Tk):
         # Add Student label
         studentBtn = Label(master, text='Add Student')
         studentBtn.config(font=("Arial Bold", 13), bg=self._green, fg=self._yellow)
-        studentBtn.place(x=348,y=465, height=27, width=106)
+        studentBtn.place(x=348, y=465, height=27, width=106)
         studentBtn.bind(self._button, self.addStudentButtonClick)
 
         logo = PhotoImage(file="./img/RegToolsLogo.gif")
-        label = Label(image = logo, borderwidth = 0)
+        label = Label(image=logo, borderwidth=0)
         label.image = logo
-        label.place(x=100,y=40)
+        label.place(x=100, y=40)
 
         # Makes RegTool title
         logoPad = Label(master, text='')
-        logoPad.place(x=95,y=40, height=5, width=620)
+        logoPad.place(x=95, y=40, height=5, width=620)
         logoPad.config(bg=self._backgroundColor)
         logoPad = Label(master, text='')
-        logoPad.place(x=98,y=40, height=90, width=5)
+        logoPad.place(x=98, y=40, height=90, width=5)
         logoPad.config(bg=self._backgroundColor)
         logoPad = Label(master, text='')
-        logoPad.place(x=701,y=40, height=90, width=5)
+        logoPad.place(x=701, y=40, height=90, width=5)
         logoPad.config(bg=self._backgroundColor)
         logoPad = Label(master, text='')
-        logoPad.place(x=95,y=121, height=5, width=620)
+        logoPad.place(x=95, y=121, height=5, width=620)
         logoPad.config(bg=self._backgroundColor)
-
 
         # Name listbox properties
-        self.lb = Listbox(fr,selectmode="browse",bg=self._backgroundColor, selectbackground=self._green)
+        self.lb = Listbox(fr, selectmode="browse", bg=self._backgroundColor, selectbackground=self._green)
         self.loadLB()
-        self.lb.config(font=("Mincho Bold", 16),fg="#e6e6e6")
-        self.lb.place(x = 5, y = 5, height = 190, width = 190)
+        self.lb.config(font=("Mincho Bold", 16), fg="#e6e6e6")
+        self.lb.place(x=5, y=5, height=190, width=190)
         self.lb.bind('<Double-Button-1>', self.onClick)
         self.lb.bind("d", self.deleteName)
 
         # "Search" Label
         searchLabel = Label(master, text='Search:')
-        searchLabel.place(x=290,y=205, height=40, width=80)
+        searchLabel.place(x=290, y=205, height=40, width=80)
         searchLabel.config(font=("Arial Bold", 16), bg=self._darkGrey, fg=self._yellow)
-
 
         self.nameVar = StringVar()
         self.search_var = StringVar()
@@ -82,18 +80,18 @@ class MainMenu(tk.Tk):
 
         # Creates the search textbox/entrybox
         searchbox = Entry(master, textvariable=self.search_var, bg=self._lightGrey)
-        searchbox.place(x=360,y=215, height=20, width=141)
+        searchbox.place(x=360, y=215, height=20, width=141)
         searchbox.config(highlightthickness=0)
 
         # 'D' Key label
         dKeyLabel = Label(master, text='= Deletes Name')
-        dKeyLabel.place(x=547,y=550, height=30, width=200)
+        dKeyLabel.place(x=547, y=550, height=30, width=200)
         dKeyLabel.config(font=("Arial Bold", 18))
         dKeyLabel.config(bg=self._backgroundColor, fg="Grey")
 
         # Key label
         dKey = Label(master, text="'D'")
-        dKey.place(x=543,y=550, height=30, width=30)
+        dKey.place(x=543, y=550, height=30, width=30)
         dKey.config(font=("Arial Bold", 20))
         dKey.config(bg="Grey", fg=self._backgroundColor)
 
