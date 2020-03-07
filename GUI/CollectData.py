f44b75460ffa18900ea588c16fd2145f2113807a
ryangurn <ryangurnick@gmail.com>
2020-03-06 21:17:31 -0800

    minor changes to the gui and ensuring owen is not off by one

--- GUI/CollectData.py
+++ GUI/CollectData.py
@@ -5,6 +5,7 @@ import StudentModel
 import ClassParser
 import ClassModel
 
+
 class collectData(tk.Tk):
     def __init__(self, master, db_file):
         self.db = db_file
@@ -19,52 +20,63 @@ class collectData(tk.Tk):
         self.cm = ClassModel.ClassModel(self.db)
         # Creates a frame to add to the master window
         self.master = master
-        self.newWindow = Frame(master, bg = self._darkGrey)
-        self.newWindow.place(x = 0, y = 150, height = 600, width = 800)
+        self.newWindow = Frame(master, bg=self._darkGrey)
+        self.newWindow.place(x=0, y=150, height=600, width=800)
 
         # Year label
         yearLabel = Label(self.newWindow, text='Select the Year')
-        yearLabel.place(x=100,y=150, height=30, width=150)
+        yearLabel.place(x=100, y=150, height=30, width=150)
         yearLabel.config(font=("Arial Bold", 18))
         yearLabel.config(bg=self._darkGrey, fg="Grey")
 
         self.yearVal = StringVar(self.newWindow)
-        #TODO: Just for testing, need to get from DB
-        yearChoices = sorted({'2016', '2017', '2018', '2019', '2020'})
-        self.yearVal.set('2019')  # Default value
+        yearChoices = sorted({'2015', '2016', '2017', '2018', '2019', '2020'})
+        self.yearVal.set('2020')  # Default value
         yearMenu = OptionMenu(self.newWindow, self.yearVal, *yearChoices)
         yearMenu.place(x=100, y=185, width=150, height=30)
         yearMenu.config(bg=self._darkGrey)
 
         # Subject label
         subjectLabel = Label(self.newWindow, text='Select the Subject')
-        subjectLabel.place(x=285,y=150, height=30, width=170)
+        subjectLabel.place(x=285, y=150, height=30, width=170)
         subjectLabel.config(font=("Arial Bold", 18))
         subjectLabel.config(bg=self._darkGrey, fg="Grey")
 
         self.subjectVal = StringVar(self.newWindow)
-        subjectChoices = ["AAAP", "AAD", "ACTG", "AEIS", "AFR", "AIM", "ANTH", "ANTM", "ARB", "ARCH", "ARH", "ART", "ARTC",
-                 "ARTD", "ARTF", "ARTM", "ARTO", "ARTP", "ARTR", "ARTS", "ASIA", "ASL", "ASTR", "BA", "BI", "BIKC",
-                 "BIOE", "BLST", "CARC", "CAS", "CDS", "CFT", "CH", "CHKC", "CHN", "CHNF", "CINE", "CIS", "CIT", "CLAS",
-                 "COLT", "CPSY", "CRDG", "CRES", "CRWR", "CSCH", "DAN", "DANC", "DANE", "DIST", "DSGN", "EALL", "EC",
-                 "ECE", "EDLD", "EDST", "EDUC", "ENG", "ENVS", "ERTH", "ES", "ESC", "EURO", "FHS", "FIN", "FINN", "FLR",
-                 "FR", "GEOG", "GEOL", "GER", "GRK", "GRST", "GSAE", "GSCL", "GSGE", "GSST", "HBRW", "HC", "HIST",
-                 "HPHY", "HUM", "IARC", "ICH", "INTL", "IST", "ITAL", "J", "JDST", "JGS", "JPN", "KC", "KRN", "LA",
-                 "LAS", "LAT", "LAW", "LEAD", "LERC", "LIB", "LING", "LT", "MATH", "MDVL", "MENA", "MGMT", "MIL",
-                 "MKTG", "MUE", "MUJ", "MUP", "MUS", "NAS", "NORW", "OBA", "OIMB", "OLIS", "PD", "PDX", "PE", "PEAQ",
-                 "PEAS", "PEC", "PEF", "PEI", "PEIA", "PEL", "PEMA", "PEMB", "PEO", "PERS", "PERU", "PETS", "PEW",
-                 "PHIL", "PHKC", "PHYS", "PORT", "PPPM", "PREV", "PS", "PSY", "QST", "REES", "REL", "RL", "RUSS",
-                 "SBUS", "SCAN", "SCYP", "SERV", "SLP", "SOC", "SPAN", "SPD", "SPED", "SPM", "SPSY", "SWAH", "SWED",
-                 "TA", "TLC", "UGST", "WGS", "WR"]
+        subjectChoices = ["AAAP", "AAD", "ACTG", "AEIS", "AFR", "AIM", "ANTH", "ANTM", "ARB", "ARCH", "ARH", "ART",
+                          "ARTC",
+                          "ARTD", "ARTF", "ARTM", "ARTO", "ARTP", "ARTR", "ARTS", "ASIA", "ASL", "ASTR", "BA", "BI",
+                          "BIKC",
+                          "BIOE", "BLST", "CARC", "CAS", "CDS", "CFT", "CH", "CHKC", "CHN", "CHNF", "CINE", "CIS",
+                          "CIT", "CLAS",
+                          "COLT", "CPSY", "CRDG", "CRES", "CRWR", "CSCH", "DAN", "DANC", "DANE", "DIST", "DSGN", "EALL",
+                          "EC",
+                          "ECE", "EDLD", "EDST", "EDUC", "ENG", "ENVS", "ERTH", "ES", "ESC", "EURO", "FHS", "FIN",
+                          "FINN", "FLR",
+                          "FR", "GEOG", "GEOL", "GER", "GRK", "GRST", "GSAE", "GSCL", "GSGE", "GSST", "HBRW", "HC",
+                          "HIST",
+                          "HPHY", "HUM", "IARC", "ICH", "INTL", "IST", "ITAL", "J", "JDST", "JGS", "JPN", "KC", "KRN",
+                          "LA",
+                          "LAS", "LAT", "LAW", "LEAD", "LERC", "LIB", "LING", "LT", "MATH", "MDVL", "MENA", "MGMT",
+                          "MIL",
+                          "MKTG", "MUE", "MUJ", "MUP", "MUS", "NAS", "NORW", "OBA", "OIMB", "OLIS", "PD", "PDX", "PE",
+                          "PEAQ",
+                          "PEAS", "PEC", "PEF", "PEI", "PEIA", "PEL", "PEMA", "PEMB", "PEO", "PERS", "PERU", "PETS",
+                          "PEW",
+                          "PHIL", "PHKC", "PHYS", "PORT", "PPPM", "PREV", "PS", "PSY", "QST", "REES", "REL", "RL",
+                          "RUSS",
+                          "SBUS", "SCAN", "SCYP", "SERV", "SLP", "SOC", "SPAN", "SPD", "SPED", "SPM", "SPSY", "SWAH",
+                          "SWED",
+                          "TA", "TLC", "UGST", "WGS", "WR"]
 
         self.subjectVal.set('CIS')
         subjectMenu = OptionMenu(self.newWindow, self.subjectVal, *subjectChoices)
         subjectMenu.config(bg=self._darkGrey)
-        subjectMenu.place(x = 290, y = 185, width = 150, height = 30)
+        subjectMenu.place(x=290, y=185, width=150, height=30)
 
         # Term label
         yearLabel = Label(self.newWindow, text='Select the Term')
-        yearLabel.place(x=485,y=150, height=30, width=150)
+        yearLabel.place(x=485, y=150, height=30, width=150)
         yearLabel.config(font=("Arial Bold", 18))
         yearLabel.config(bg=self._darkGrey, fg="Grey")
 
@@ -73,20 +85,18 @@ class collectData(tk.Tk):
         self.termVal.set('Spring')
         termMenu = OptionMenu(self.newWindow, self.termVal, *termChoices)
         termMenu.config(bg=self._darkGrey)
-        termMenu.place (x = 490, y = 185, width = 150, height = 30)
-
-
+        termMenu.place(x=490, y=185, width=150, height=30)
 
         # Button to trigger the data collection
         collectData = Label(self.newWindow, text='Collect Data')
         collectData.config(font=("Arial Bold", 18), bg="#369148", fg=self._yellow)
-        collectData.place(x=225, y= 340, height=30, width=140)
+        collectData.place(x=225, y=340, height=30, width=140)
         collectData.bind("<Double-Button-1>", self.dataCollectClick)
 
         # Button to close the "Select Data" window and go back to main menu
         exitName = Label(self.newWindow, text='Return Home')
         exitName.config(font=("Arial Bold", 18), bg="#369148", fg=self._yellow)
-        exitName.place(x=410, y= 340, height=30, width=140)
+        exitName.place(x=410, y=340, height=30, width=140)
         exitName.bind("<Double-Button-1>", self.exitWindow)
 
     def exitWindow(self, event):
@@ -100,22 +110,21 @@ class collectData(tk.Tk):
         if term == "Fall":
             t = 1
         elif term == "Winter":
-            #y -= 1
+            y -= 1
             t = 2
         elif term == "Spring":
-            #y -= 1
+            y -= 1
             t = 3
         elif term == "Summer":
-            #y -= 1
+            y -= 1
             t = 4
 
         ye = str(y)
         s = ye + "0" + str(t)
-        currentClasses = self.cm.find_by_term(self.subjectVal.get(), year, t)
+        currentClasses = self.cm.find_by_term(self.subjectVal.get(), y, t)
         if len(currentClasses):
             self.cm.delete_sub_term(self.subjectVal.get(), s)
 
         p = ClassParser.ClassParser(s, self.subjectVal.get())
         p.deleteFormatting()
         p.parseData()
-        
